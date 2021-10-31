856cfebe33acac76cd532bcd469b07fc1c563e77
Radoslaw Taborski <radoslawtaborski@gmail.com>
2021-10-31 17:56:10 +0100

    tests pass

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Something else";
+            return "Rado";
         }
 
         [HttpPost]
