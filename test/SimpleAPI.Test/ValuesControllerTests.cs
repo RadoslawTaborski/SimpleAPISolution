70703cc18e0f0dc6fd41b432b21abf32efcf5904
RadoslawTaborski <radoslaw.taborski@gmail.com>
2022-02-22 18:04:54 +0100

    Update ValuesControllerTests.cs

--- test/SimpleAPI.Test/ValuesControllerTests.cs
+++ test/SimpleAPI.Test/ValuesControllerTests.cs
@@ -13,7 +13,7 @@ namespace SimpleAPI.Test
         public void Get_ReturnsMyName()
         {
             var returnValue = controller.Get(1);
-            Assert.Equal("Rado", returnValue.Value);
+            Assert.Equal("Rado", returnValue.Value); 
         } 
 
         [Fact]
